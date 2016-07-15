e2615b0a831a7675931de321a1d6650a4ceba59b
Poseidon <wangliyingwad@hotmail.com>
2016-07-15 18:23:38 +0800

    INITIAL

--- bluetoothlib/src/main/java/com/bluetooth/leo/bluetoothlib/BluetoothManager.java
+++ bluetoothlib/src/main/java/com/bluetooth/leo/bluetoothlib/BluetoothManager.java
@@ -1,11 +1,15 @@
 package com.bluetooth.leo.bluetoothlib;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2016/7/15.
  */
 public class BluetoothManager {
     private BluetoothAdapter bluetoothAdapter;
-    
+    private List<BluetoothDevice> devices;
+
 }
