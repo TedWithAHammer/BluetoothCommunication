f07b01a49a6edf202fa4dbd4fc41c58688dce7cd
Poseidon <wangliyingwad@hotmail.com>
2016-07-15 18:19:02 +0800

    Initial commit

---
+++ bluetoothlib/src/main/java/com/bluetooth/leo/bluetoothlib/BluetoothManager.java
@@ -0,0 +1,11 @@
+package com.bluetooth.leo.bluetoothlib;
+
+import android.bluetooth.BluetoothAdapter;
+
+/**
+ * Created by Administrator on 2016/7/15.
+ */
+public class BluetoothManager {
+    private BluetoothAdapter bluetoothAdapter;
+    
+}
